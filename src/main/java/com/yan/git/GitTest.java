1b02f5804efeea2798a04ee9c71ac1edef8ae0a7
yandu <1341543810@qq.com>
2022-07-19 10:28:46 +0800

    first commit

---
+++ src/main/java/com/yan/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.yan.git;
+
+/**
+ * @author yandu
+ * @create 2022-07-19 10:22
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
